e79dce8b35441778a64c81931c0e674e678eb99d
Tyler Breisacher <matrixfrog@gmail.com>
2011-01-09 20:48:43 -0800

    lots of new icons

--- stackoverflow-icons.user.js
+++ stackoverflow-icons.user.js
@@ -6,6 +6,7 @@
 // ==/UserScript==
 
 var icons = {
+  django : 'http://www.djangoproject.com/favicon.ico',
   eclipse : 'http://eclipse.org/favicon.ico',
   'internet-explorer' : 'http://ie.microsoft.com/testdrive/Graphics/CanvasPad/images/ie_small.png',
   firefox : 'http://www.mozilla.com/favicon.ico',
@@ -13,11 +14,20 @@ var icons = {
   git : 'http://git-scm.com/favicon.png',
   github : 'https://github.com/favicon.ico',
   haskell : 'http://www.haskell.org/favicon.ico',
+  ios : 'http://itunes.apple.com/favicon.ico',
+  iphone : 'http://itunes.apple.com/favicon.ico',
   java : 'http://java.sun.com/favicon.ico', 
   jquery : 'http://static.jquery.com/favicon.ico',
+  mercurial : 'http://mercurial.selenic.com/favicon.ico',
+  mysql : 'http://www.mysql.com/favicon.ico',
+  osx : 'http://images.apple.com/macosx/what-is-macosx/images/subnav_overview20090824.png',
+  perl : 'http://www.perl.org/favicon.ico',
   php : 'http://www.php.net/favicon.ico',
+  'project-euler' : 'http://projecteuler.net/favicon.ico',
   python : 'http://python.org/favicon.ico',
+  ruby : 'http://www.ruby-lang.org/favicon.ico',
   svn : 'http://subversion.tigris.org/favicon.ico',
+  windows : 'http://www.microsoft.com/windows/Framework/images/favicon.ico',
   xul : 'http://www.mozilla.org/favicon.ico'
 };
 
